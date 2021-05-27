d43ff673910a08d22ef46eec33f91ad72e9bd6af
frandferrari <frandferrari@outlook.com>
2021-05-27 18:07:35 -0300

    LocationStats

---
+++ src/main/java/com/example/coronavirustracker/models/LocationStats.java
@@ -0,0 +1,39 @@
+package com.example.coronavirustracker.models;
+
+public class LocationStats {
+
+	private String state;
+	private String country;
+	private int latestTotalCases;
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public int getLatestTotalCases() {
+		return latestTotalCases;
+	}
+
+	public void setLatestTotalCases(int latestTotal) {
+		this.latestTotalCases = latestTotal;
+	}
+
+	@Override
+	public String toString() {
+		return "LocationStats [state=" + state + ", country=" + country + ", latestTotalCases=" + latestTotalCases
+				+ "]";
+	}
+
+}
